4ffdc79cc083c4aa69ca98f10a4cc328f06271ae
Joshua Marshall Moore <joshua.marshall.moore@gmail.com>
2010-07-25 12:43:13 -0700

    Initial Import

---
+++ masks.py
@@ -0,0 +1,43 @@
+# masks.py
+# Joshua Marshall Moore
+# 7/24/2010
+
+import sqlite3
+_DB_PATH = "C:\hope\www\masks\masks.db"
+_conn = sqlite3.connect(_DB_PATH)
+_c = _conn.cursor()
+_c.execute("""create table if not exists ring (id int primary key, name text)""")
+_c.execute("""create table if not exists person (id int primary key, name text)""")
+_c.execute("""create table if not exists linkRingPerson(ringid int, personid int)""")
+_conn.commit()
+_c.close()
+
+
+def getRings(req):
+    conn = sqlite3.connect(_DB_PATH)
+    c = conn.cursor()
+    rings = c.execute("""select id, name from ring""")
+
+    sendstring = """{"rings": ["""
+
+    first = True
+    for ring in rings:
+        if not first:
+            sendstring += ','
+        sendstring += '{"id": "' + str(ring[0]) + '",'
+        sendstring += '"name": "' + ring[1] + '"}'
+        first = False
+
+    sendstring += """]}"""
+    c.close()
+    return sendstring
+
+def addRing(req):
+    f = req.form
+    ringname = f['ringname']
+
+    conn = sqlite3.connect(_DB_PATH)
+    c = conn.cursor()
+    c.execute("""insert into ring (id, name) values (NULL, ?)""", (ringname, ))
+    conn.commit()
+    c.close()
---
+++ scripts.js
@@ -0,0 +1,38 @@
+function loadRings()
+{
+    var ringSelect = document.getElementById("ringSelect");
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/masks/masks.py/getRings", true);
+    xhr.onreadystatechange = function() {
+	if (xhr.readyState==4 && xhr.status==200){
+
+	    // remove existing options
+	    while(ringSelect.options.length){
+		ringSelect.options.remove(0);
+	    }
+
+	    // add options retrieved from server 
+	    var obj = json_parse(xhr.responseText);
+	    for (var i=0; i<obj.rings.length; i++){
+		ringSelect.options.add(new Option(obj.rings[i].name,
+						  obj.rings[i].id));
+	    }
+	}
+    }
+    xhr.send();
+}
+
+function addRing()
+{
+    var ringEntry = document.getElementById("ringEntry");
+    var xhr = new XMLHttpRequest();
+    var params = "ringname=" + ringEntry.value;
+    ringEntry.value = "";
+    xhr.open("POST", "/masks/masks.py/addRing", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {}
+    xhr.send(params);
+
+    loadRings();
+}
