a11816b8ee0c958f01475deb8ee22f71095d2356
Joshua Marshall Moore <joshua.marshall.moore@gmail.com>
2010-07-25 22:45:32 -0700

    Attempted to add Person related functionality

--- masks.py
+++ masks.py
@@ -43,12 +43,80 @@ def deleteRing(req):
     conn.commit()
     c.close()
 
+
 def addRing(req):
     f = req.form
     ringname = f['ringname']
 
     conn = sqlite3.connect(_DB_PATH)
     c = conn.cursor()
+
+    # prevent duplicate entries
+    ringcheck = c.execute("""select id, name from ring where name=?""", 
+                          (ringname, ))
+    if ringcheck.rowcount > 0:
+        c.close()
+        return
+
     c.execute("""insert into ring (id, name) values (NULL, ?)""", (ringname, ))
     conn.commit()
     c.close()
+
+
+def getPersons(req):
+    conn = sqlite3.connect(_DB_PATH)
+    c = conn.cursor()
+    persons = c.execute("""select id, name from person""")
+
+    sendstring = """{"persons": ["""
+    first = True
+    for person in persons:
+        if not first:
+            sendstring += ','
+        sendstring += '{"id": "' + str(person[0]) + '",'
+        sendstring += '"name": "' + person[1] + '"}'
+        first = False
+    
+    sendstring += """]}"""
+    c.close()
+    return sendstring
+
+
+def deletePersonFromRing(req):
+    f = req.form
+    ringid = int(f['ringid'])
+    personid = int(f['personid'])
+    conn = sqlite3.connect(_DB_PATH)
+    c = conn.cursor()
+    
+    c.execute("""delete from linkRingPerson where ringid=? and personid=?""", 
+              (ringid, personid))
+    conn.commit()
+    c.close()
+
+
+def addPersonToRing(req):
+    f = req.form
+    conn = sqlite3.connect(_DB_PATH)
+    c = conn.cursor()
+    
+    personname = f['personname']
+    ringid = f['ringid']
+
+    # prevent duplicate entries in person table
+    personcheck = c.execute("""select id from person where name=?""",
+                            (personname, ))
+    personid = None
+    if not personcheck.rowcount:
+        personid = personcheck[0][0]
+    else:
+        persons = c.execute("""insert into person values (NULL, ?)""", (personname, ))
+
+        personid = None
+        persons = c.execute("""select id from person where name=?""", (personname, ))
+        for person in persons:
+            personid = int(person[0])
+    # prevent duplicate entries in linkRingPerson table
+    linkcheck = c.execute("""select ringid, personid from linkRingPerson where ringid=? and personid=?""", (ringid, personid))
+    if not linkcheck.rowcount:
+        c.execute("""insert into linkRingPerson (ringid, personid) values (?, ?)""", (ringid, personid))
--- scripts.js
+++ scripts.js
@@ -55,3 +55,79 @@ function addRing()
     // reload rings
     loadRings();
 }
+
+
+function loadPersons()
+{
+    var ringSelect = document.getElementById('ringSelect');
+    if(ringSelect.selectedIndex==-1){
+	return;
+    }
+    var personSelect = document.getElementById('personSelect');
+    
+    var xhr = new XMLHttpRequest();
+    var params = "ringid=" + ringSelect[ringSelect.selectedIndex].value;
+    xhr.open("GET", "/masks/masks.py/getPersons", true);
+    xhr.onreadystatechange = function() {
+	if(xhr.readyState==4 && xhr.status==200){
+	    // remove existing options
+	    while(personSelect.options.length){
+		personSelect.options.remove(0);
+	    }
+
+	    // add options retrieved from server
+	    var obj = json_parse(xhr.responseText);
+	    for(var i=0; i<obj.persons.length; i++){
+		personSelect.options.add(new Option(obj.persons[i].name,
+						    obj.persons[i].id));
+	    }
+	}
+    }
+    xhr.send();
+}
+
+
+function deletePersonFromRing()
+{
+    var personSelect = document.getElementById('personSelect');
+    var ringSelect = document.getElementById('ringSelect');
+
+    if (ringSelect.selectedIndex == -1){
+	return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    var params = "personid=" +
+	personSelect[personSelect.selectedIndex].value.toString() + 
+	"&ringid=" + ringSelect[ringSelect.selectedIndex].value.toString();
+    xhr.open("POST", "/masks/masks.py/deletePersonFromRing", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {}
+    xhr.send(params);
+
+    loadPersons();
+}
+
+
+function addPersonToRing()
+{
+    var personEntry = document.getElementById('personEntry');
+    if (personEntry.value==null || personEntry.value==""){
+	return;
+    }
+    var ringSelect = document.getElementById('ringSelect');
+    if (ringSelect.selectedIndex==-1){
+	return;
+    }
+    var xhr = new XMLHttpRequest();
+    var params = "ringid=" + ringSelect[ringSelect.selectedIndex].value.toString() + 
+	"&personname=" + personEntry.value;
+    personEntry.value = ""
+    xhr.open("POST", "/masks/masks.py/addPersonToRing", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadstatechange = function(){}
+    xhr.send(params);
+
+    // reload persons
+    loadPersons();
+}
